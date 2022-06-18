64b021f24726d970fa701db8f2bf48ab5edbf2d1
Ronaldo Barajas <rbarajas_8@hotmail.com>
2022-06-18 09:33:06 -0700

    part 3 done

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-function newImage(link, left, bottom){
+function newImage(link, left, bottom) {
     let item = document.createElement('img')
     item.src = link
     item.style.position = 'fixed'
     item.style.left = left + 'px'
-    item.style.bottom = bottom + 'px'
+    item.style.bottom = bottom + 'px' 
     document.body.append(item)
     return item
 }
-function newItem(link, left, bottom){
-let equipment = newImage(link, left, bottom)
 
-equipment.addEventListener('dblclick', function(){
-    equipment.remove()
-})
+function newItem(link, left, bottom) {
+    let equipment = newImage(link, left, bottom)
+    equipment.addEventListener('click', function(){
+        equipment.remove();
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = link
+        inventory.append(inventoryItem)
+    })
+}
+let inventory = document.createElement('div')
+
+function newInventory() {
+inventory.style.position = 'fixed'
+inventory.style.bottom = '0px'
+inventory.style.left = '0px'
+inventory.style.width = '400px'
+inventory.style.height = '100px'
+inventory.style.display = 'flex'
+inventory.style.flexDirection = 'row'
+inventory.style.alignItems = 'center'
+inventory.style.justifyContent = 'space-evenly'
+inventory.style.border = '2px solid black'
+inventory.style.backgroundColor = 'brown'
+document.body.append(inventory)
 }
 
-newItem('assets/sword.png',570,405)
+function move(image) {
+    image.style.position = 'fixed'
+    
+    function moveToCoordiantes(left,bottom) {
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+
+    return{
+        to: moveToCoordiantes
+    }
+}
+
+
+
+
+newInventory()
+newItem('assets/sword.png',300,700)
 newItem('assets/sheild.png',165,185)
 newItem('assets/staff.png',600,100)
 
-
-
-newImage('assets/pine-tree.png',450, 200)
-newImage('assets/green-character.gif',100,100)
-newImage('assets/Tree.png',200,300)
-newImage('assets/pillar.png',350,100)
-newImage('assets/crate.png',150,200)
-newImage('assets/well.png',500,425)
+move(newImage('assets/pine-tree.png')).to(450,200)
+move(newImage('assets/green-character.gif')).to(100,250)
+move(newImage('assets/Tree.png')).to(200,300)
+move(newImage('assets/pillar.png')).to(350,100)
+move(newImage('assets/crate.png')).to(150,200)
+move(newImage('assets/well.png')).to(500,425)
